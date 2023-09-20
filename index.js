12ef6d13d4258bf2db982edfc976777c56f2b446
brustcraft458 <84393872+brustcraft458@users.noreply.github.com>
2023-09-20 17:51:38 +0700

    Tes Websocket

--- index.js
+++ index.js
@@ -2,25 +2,23 @@
 import express from "express"
 import { WebSocketServer } from "ws"
 
-const wss = new WebSocketServer({port: 3000})
-
 // Middlewares
 const app = express();
 app.use(express.json());
 
-// Websocket Tess
-wss.on("connection", (ws) => {
-  ws.on("message", async(msg) => {
-    ws.send(`hello ${msg}`)
-  })
-})
-
 // Root
 app.get("/sample", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Hello Root",
-    message: "Api Rest",
-  });
+  try {
+    const wss = new WebSocketServer({port: 3000})
+    // Websocket Tess
+    wss.on("connection", (ws) => {
+      ws.on("message", async(msg) => {
+        ws.send(`hello ${msg}`)
+      })
+    })
+  } catch (error) {
+    res.status(200).json(JSON.stringify(error))
+  }
 });
 
 // connection
