10ace68498aa51b233eb955f8a1582d2141833c5
brustcraft458 <84393872+brustcraft458@users.noreply.github.com>
2023-09-20 13:30:47 +0700

    Update

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // Import packages
 const express = require("express");
-const home = require("./routes/home");
 
 // Middlewares
 const app = express();
 app.use(express.json());
 
-// Routes
-app.use("/home", home);
+// Root
+app.get("/sample", async (req, res, next) => {
+  return res.status(200).json({
+    title: "Hello Root",
+    message: "Api Message",
+  });
+});
 
 // connection
 const port = process.env.PORT || 9001;
